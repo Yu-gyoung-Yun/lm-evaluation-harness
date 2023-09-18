01dc1cb17c7e582729a717c304fd7e6f87352ccf
Yu-gyoung-Yun <74442792+Yu-gyoung-Yun@users.noreply.github.com>
2023-09-18 13:04:20 +0900

    Create gpt2_offload.py

---
+++ lm_eval/models/gpt2_offload.py
@@ -0,0 +1,314 @@
+# This might be slower than deepspeed.inference
+import torch
+import transformers
+from typing import Optional, Union
+from lm_eval.base import BaseLM
+import os
+import gc
+import deepspeed
+from deepspeed import comm as dist
+from transformers.deepspeed import HfDeepSpeedConfig
+from deepspeed.accelerator import get_accelerator
+from transformers import AutoConfig
+KB = 1 << 10
+MB = 1 << 20
+GB = 1 << 30
+T = 1e12
+import sys
+sys.set_int_max_str_digits(5000000)
+
+def model_bytes(config):
+    h = config.hidden_size
+    return 	2 * (config.num_hidden_layers * (
+    # config-attention
+    h * (3 * h + 1) + h * (h + 1) +
+    # mlp
+    h * (4 * h + 1) + h * 4 * (h + 1) +
+    # layer norm
+    h * 4) +
+    # embedding
+    config.vocab_size * (h + 1))
+
+def cache_bytes(config, batch_size, seq_len):
+    return 2 * batch_size * seq_len * config.num_hidden_layers * config.hidden_size * 2
+
+def hidden_bytes(config, batch_size, seq_len=200):
+    return batch_size * seq_len * config.hidden_size * 2
+
+def _get_dtype(
+    dtype: Union[str, torch.dtype]
+) -> torch.dtype:
+    """Converts `dtype` from `str` to torch.dtype when possible. Does not use an instantiated HF AutoConfig"""
+    if isinstance(dtype, str) and dtype != "auto":
+        # Convert `str` args torch dtype: `float16` -> `torch.float16`
+        _torch_dtype = getattr(torch, dtype)
+    else:
+        _torch_dtype = dtype
+    return _torch_dtype
+
+
+class HFLM(BaseLM):
+
+    _DEFAULT_MAX_LENGTH = 2048
+
+    def __init__(
+        self,
+        device="cuda",
+        pretrained="gpt2",
+        revision="main",
+        low_cpu_mem_usage=None,
+        subfolder=None,
+        tokenizer=None,
+        batch_size=1,
+        max_batch_size=512,
+        max_length=None,
+        load_in_8bit: Optional[bool] = False,
+        trust_remote_code: Optional[bool] = False,
+        dtype: Optional[Union[str, torch.dtype]]="auto",
+    ):
+        super().__init__()
+
+        deepspeed.init_distributed("nccl")
+        rank = dist.get_rank()
+        pin_memory = True #bool(args.pin_memory)
+        dtype = torch.float16
+
+        #cache_size = cache_bytes(config, batch_size, prompt_len + gen_len)
+        #config = AutoConfig.from_pretrained(pretrained) #model_name)
+        #hidden_size = int(hidden_bytes(config, batch_size) )#, prompt_len + gen_len)
+        
+        print(f"batch_size: {batch_size}")
+        ds_config = {
+            "fp16": {
+                "enabled": dtype == torch.float16,
+            },
+            "bf16": {
+                "enabled": dtype == torch.bfloat16,
+            },
+            "zero_optimization": {
+                "stage": 3,
+                #"stage3_prefetch_bucket_size": 2 * hidden_size * hidden_size, # 0, 
+                #"stage3_param_persistence_threshold": hidden_size,
+                #"stage3_max_live_parameters": 2 * hidden_size * hidden_size,
+                "overlap_comm": True
+            },
+            "steps_per_print": 2000,
+            "train_batch_size": int(batch_size),
+            "train_micro_batch_size_per_gpu": int(batch_size)/int(dist.get_world_size()),
+            "wall_clock_breakdown": False,
+        }
+
+        '''if bits == 4:
+            quant_config = get_quant_config(config, bits=bits, group_size=group_size)
+            ds_config.update(quant_config)'''
+        disk_offload=True
+        cpu_offload=False
+        if cpu_offload:
+            ds_config["zero_optimization"]["offload_param"] = dict(
+                device="cpu", pin_memory=pin_memory
+            )
+
+        if disk_offload:
+            ds_config["zero_optimization"]["offload_param"] = dict(
+                device="nvme",
+                pin_memory=True, #pin_memory,
+                nvme_path="dev/nvme0", #offload_dir,
+                buffer_count=7, #5,
+                buffer_size=1e9# 2 * GB #9 * GB if config.model_type == 'bloom' else 2 * GB,
+            )
+            # apt-get install libaio-dev
+            '''ds_config["aio"] = {
+                "block_size": 1048576,
+                "queue_depth": 8,
+                "thread_count": 1,
+                "single_submit": False,
+                "overlap_events": True,
+            }'''
+
+        dschf = HfDeepSpeedConfig(
+            ds_config
+        )  # this tells from_pretrained to instantiate directly on gpus
+
+        # Initialize model
+        if isinstance(pretrained, transformers.PreTrainedModel):
+            self.model = pretrained
+            self._device = self.model.device
+
+            if tokenizer:
+                assert isinstance(
+                        tokenizer,
+                        transformers.PreTrainedTokenizer
+                        ) or isinstance(
+                        tokenizer,
+                        transformers.PreTrainedTokenizerFast
+                        )
+                self.tokenizer = tokenizer
+            else:
+                # Get tokenizer
+                model_name = self.model.name_or_path
+                self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+                        model_name,
+                        revision=revision,
+                        trust_remote_code=trust_remote_code,
+                        use_fast=False,
+                        )
+
+        elif isinstance(pretrained, str):
+
+            # Initialize device
+            assert isinstance(device, str)
+            device_list = set(
+                ["cuda", "cpu"] + [f"cuda:{i}" for i in range(torch.cuda.device_count())]
+            )
+            if device and device in device_list:
+                self._device = torch.device(device)
+                print(f"Using device '{device}'")
+            else:
+                print("Device not specified")
+                print(f"Cuda Available? {torch.cuda.is_available()}")
+                self._device = (
+                    torch.device("cuda")
+                    if torch.cuda.is_available()
+                    else torch.device("cpu")
+                )
+            revision = revision + ("/" + subfolder if subfolder is not None else "")
+
+            # Initialize new model and tokenizer instances
+            self.model = transformers.AutoModelForCausalLM.from_pretrained(
+                    pretrained,
+                    #load_in_8bit=load_in_8bit,
+                    #low_cpu_mem_usage=low_cpu_mem_usage,
+                    revision=revision,
+                    torch_dtype=torch.float16, #_get_dtype(dtype),
+                    trust_remote_code=trust_remote_code,
+                    )#.to(self.device)
+            self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+                    tokenizer if tokenizer else pretrained,
+                    revision=revision,
+                    trust_remote_code=trust_remote_code,
+                    use_fast = False,
+                    )
+
+        else:
+            raise TypeError('Parameter pretrained should be of type str or transformers.PreTrainedModel')
+
+        self.model.eval()
+        local_rank = int(os.getenv('LOCAL_RANK', '0'))
+        #world_size = int(os.getenv('WORLD_SIZE', '2'))
+        world_size = int(os.environ['WORLD_SIZE'])
+
+        print(f"Please check the world_size: {world_size}")
+        '''zero_config = {
+            #"kernel_inject": False,
+            "tensor_parallel": {"tp_size": world_size},
+            "dtype": torch.half,
+            #"enable_cuda_graph": False
+        }'''
+        '''self.model = deepspeed.init_inference(self.model,
+                                #mp_size=world_size,
+                                dtype=torch.half,
+                                config=zero_config,
+                                #replace_policy=LLAMALayerPolicy
+        )'''
+        '''self.model = deepspeed.init_inference(self.model,
+                                mp_size=world_size,
+                                dtype=torch.half,
+                                #use_triton=True,
+                                #config=zero_config,
+                                #replace_policy=LLAMALayerPolicy,
+                                replace_with_kernel_inject=True) # --> if This is True then, there's no AutoTP
+        '''
+        # clear cache / free memory
+        get_accelerator().empty_cache()
+        gc.collect()
+
+        '''for name, param in self.model.named_parameters():
+            if param.dtype == torch.float16:
+                print(f"Parameter {name} is of dtype torch.half (float16).")'''
+
+        ds_engine = deepspeed.initialize(model=self.model, config_params=ds_config)[0]
+        ds_engine.module.eval()
+        model = ds_engine.module
+        print(f"model.config = {model.config}")
+
+        #print(f"self.model.parameters()[0].data.dtype: {self.model.model.parameters()[0].data.dtype}")
+        print(f"deepspeed.get_accelerator().current_device_name(): {deepspeed.get_accelerator().current_device_name()}")
+        torch.cuda.set_device(deepspeed.get_accelerator().current_device_name())
+
+        self.vocab_size = self.tokenizer.vocab_size
+
+        # Validate batch_size
+        assert isinstance(batch_size, (int, str))
+
+        # setup for automatic batch size detection
+        if str(batch_size).startswith("auto"):
+            batch_size = batch_size.split(":")
+            self.batch_size_per_gpu = batch_size[0]
+            self.batch_schedule = float(batch_size[1]) if len(batch_size) > 1 else 1
+        else:
+            self.batch_size_per_gpu = int(batch_size)
+        self.max_batch_size = max_batch_size
+
+        self._max_length = max_length
+
+    @property
+    def eot_token_id(self):
+        # we use EOT because end of *text* is more accurate for what we're doing than end of *sentence*
+        return self.tokenizer.eos_token_id
+
+    @property
+    def max_length(self):
+        if self._max_length: # if max length manually set, return it
+            return self._max_length
+        seqlen_config_attrs = ("n_positions", "max_position_embeddings", "n_ctx")
+        for attr in seqlen_config_attrs:
+            if hasattr(self.model.config, attr):
+                return getattr(self.model.config, attr)
+        if hasattr(self.tokenizer, "model_max_length"):
+            if self.tokenizer.model_max_length == 1000000000000000019884624838656:
+                return self._DEFAULT_MAX_LENGTH
+            return self.tokenizer.model_max_length
+        return self._DEFAULT_MAX_LENGTH
+
+
+    @property
+    def max_gen_toks(self):
+        return 256
+
+    @property
+    def batch_size(self):
+        # TODO: fix multi-gpu
+        return self.batch_size_per_gpu  # * gpus
+
+    @property
+    def device(self):
+        # TODO: fix multi-gpu
+        return self._device
+
+    def tok_encode(self, string: str):
+        return self.tokenizer.encode(string, add_special_tokens=False)
+
+    def tok_decode(self, tokens):
+        return self.tokenizer.decode(tokens)
+
+    def _model_call(self, inps, inplens=None):
+        """
+        inps: a torch tensor of shape [batch, sequence]
+        the size of sequence may vary from call to call
+
+        returns: a torch tensor of shape [batch, sequence, vocab] with the
+        logits returned from the model
+        """
+        with torch.no_grad():
+            return self.model(inps, inplens=inplens)[0]
+
+    def _model_generate(self, context, max_length, eos_token_id):
+        generation_kwargs = {"do_sample": False, "max_length": max_length}
+        if eos_token_id is not None:
+            generation_kwargs['eos_token_id'] = eos_token_id
+            generation_kwargs['pad_token_id'] = eos_token_id # setting eos_token_id as pad token
+        return self.model.generate(context, **generation_kwargs)
+
+
+# for backwards compatibility
+GPT2LM = HFLM
